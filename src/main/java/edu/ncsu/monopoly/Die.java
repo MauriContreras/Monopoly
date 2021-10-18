cc69269d6e9fcb0f525aa8510cf8a8f053150044
porkipig <mauricg10@gmail.com>
2021-10-18 06:36:00 +0200

    some fixes

--- src/main/java/edu/ncsu/monopoly/Die.java
+++ src/main/java/edu/ncsu/monopoly/Die.java
@@ -1,9 +1,25 @@
 package edu.ncsu.monopoly;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class Die {
 
-	public int getRoll() {
+	private Random rand;
+	public static final int someInt = 6;
 
-		return (int) (Math.random() * 6) + 1;
+	public int getRoll() {
+		getRand();
+		return (this.rand.nextInt() * someInt) + 1;
+	}
+
+	public void getRand() {
+		try {
+			this.rand = SecureRandom.getInstanceStrong();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 }
